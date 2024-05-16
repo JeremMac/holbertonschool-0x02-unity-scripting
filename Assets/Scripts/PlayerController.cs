6c9d83aadee5b2a6d57a00e1237b9b01f88a3fce
JeremMac <134691915+JeremMac@users.noreply.github.com>
2024-05-16 17:53:22 +0200

    Added movement script, camera script and coin.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody PlayerRb;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetKey("up")) {
+			PlayerRb.AddForce(0 ,0 ,speed * Time.deltaTime);
+		}
+		if (Input.GetKey("down")) {
+			PlayerRb.AddForce(0 ,0 ,-speed * Time.deltaTime);
+		}
+		if (Input.GetKey("left")) {
+			PlayerRb.AddForce(-speed * Time.deltaTime ,0 ,0);
+		}
+		if (Input.GetKey("right")) {
+			PlayerRb.AddForce(speed * Time.deltaTime ,0 ,0);
+		}
+	}
+}
